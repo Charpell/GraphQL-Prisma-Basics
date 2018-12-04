a845791f4b29435e8bbbabbdc3127311ca518bf6
Charpell <charpellumeh@gmail.com>
2018-12-04 19:30:18 +0100

    Setup graphql-yoga and write API

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,30 @@
-console.log('Hello GraphQL')
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definations (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+  }
+`
+
+// Resplvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!'
+    },
+    name() {
+      return 'Ebuka Umeh'
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => {
+  console.log('The server is up!')
+})
