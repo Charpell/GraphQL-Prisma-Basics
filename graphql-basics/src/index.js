f0bfb740d22e7a4cbd8905f6f1bc24dc84264316
Charpell <charpellumeh@gmail.com>
2018-12-04 22:34:04 +0100

    CreatePost Mutation

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -79,6 +79,7 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
   }
 
   type User {
@@ -173,6 +174,26 @@ const resolvers = {
       users.push(user)
 
       return user
+    },
+
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some(user => user.id === args.author)
+
+      if (!userExists) {
+        throw new Error('User not found')
+      }
+
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author
+      }
+
+      posts.push(post)
+
+      return post
     }
   },
 
