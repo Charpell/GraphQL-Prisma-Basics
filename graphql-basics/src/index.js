2abcc778fbd3225076dc24bc37bd24e2099f4381
Charpell <charpellumeh@gmail.com>
2018-12-04 22:40:35 +0100

    CreateComment Mutation

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -80,6 +80,8 @@ const typeDefs = `
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
     createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
+
   }
 
   type User {
@@ -194,6 +196,25 @@ const resolvers = {
       posts.push(post)
 
       return post
+    },
+    createComment(parent, args, ctx, info){
+        const userExists = users.some((user) => user.id === args.author)
+        const postExists = posts.some((post) => post.id === args.post && post.published)
+
+        if (!userExists || !postExists) {
+            throw new Error('Unable to find user and post')
+        }
+
+        const comment = {
+            id: uuidv4(),
+            text: args.text,
+            author: args.author,
+            post: args.post
+        }
+
+        comments.push(comment)
+
+        return comment
     }
   },
 
