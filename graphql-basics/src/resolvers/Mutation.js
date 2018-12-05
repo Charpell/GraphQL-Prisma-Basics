5f2c543893826a09a03edc0c14a0f3ed86f047d6
Charpell <charpellumeh@gmail.com>
2018-12-05 15:30:23 +0100

    Expanding Comment Subscriptions for Edit and Deletion

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -184,24 +184,36 @@ const Mutation = {
       }
 
       db.comments.push(comment)
-      pubsub.publish(`comment ${args.data.post}`, { comment })
+      pubsub.publish(`comment ${args.data.post}`, { 
+          comment: {
+              mutation: 'CREATED',
+              data: comment
+          }
+       })
 
       return comment
   },
 
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db , pubsub }, info) {
       const commentIndex = db.comments.findIndex((comment) => comment.id === args.id)
 
       if (commentIndex === -1) {
           throw new Error('Comment not found')
       }
 
-      const deletedComments = db.comments.splice(commentIndex, 1)
+      const [ deletedComment ] = db.comments.splice(commentIndex, 1)
 
-      return deletedComments[0]
+      pubsub.publish(`comment ${deletedComment.post}`, {
+          comment: {
+              mutation: 'DELETED',
+              data: deletedComment
+          }
+      })
+
+      return deletedComment
   },
   
-  updateComment(parent, args, { db }, info) {
+  updateComment(parent, args, { db, pubsub }, info) {
       const { id, data } = args
       const comment = db.comments.find((comment) => comment.id === id)
 
@@ -212,6 +224,13 @@ const Mutation = {
       if (typeof data.text === 'string') {
           comment.text = data.text
       }
+
+      pubsub.publish(`comment ${comment.post}`, {
+          comment: {
+              mutation: 'UPDATED',
+              data: comment
+          }
+      })
 
       return comment
   }
