24c28559577f8a12591f4ace1923327de7206168
Charpell <charpellumeh@gmail.com>
2018-12-05 14:41:25 +0100

    Setting up Post Subscriptions

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -70,7 +70,7 @@ const Mutation = {
     return user
   },
 
-  createPost(parent, args, { db }, info) {
+  createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author)
 
     if (!userExists) {
@@ -83,6 +83,10 @@ const Mutation = {
     }
 
     db.posts.push(post)
+
+    if (args.data.published) {
+        pubsub.publish('post', { post })
+    }
 
     return post
   },
