fb2973332b33ba5d03de58d181e156458a896456
Charpell <charpellumeh@gmail.com>
2018-12-05 09:49:00 +0100

    Update Post Mutation

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -101,6 +101,29 @@ const Mutation = {
     return deletedPosts[0]
   },
 
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args
+    const post = db.posts.find((post) => post.id === id)
+
+    if (!post) {
+        throw new Error('Post not found')
+    }
+
+    if (typeof data.title === 'string') {
+        post.title = data.title
+    }
+
+    if (typeof data.body === 'string') {
+        post.body = data.body
+    }
+
+    if (typeof data.published === 'boolean') {
+        post.published = data.published
+    }
+
+    return post
+},
+
   createComment(parent, args, { db }, info){
       const userExists = db.users.some((user) => user.id === args.data.author)
       const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
