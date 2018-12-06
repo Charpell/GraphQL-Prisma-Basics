16401bca539a59f8ef06e51e352849f062951c76
Charpell <charpellumeh@gmail.com>
2018-12-06 08:12:01 +0100

    Copy project files into Prisma folder

---
+++ graphql-prisma/src/resolvers/Query.js
@@ -0,0 +1,47 @@
+const Query = {
+  users(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.users
+    }
+
+    return db.users.filter(user => {
+      return user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+    })
+  },
+
+  posts(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.posts
+    }
+
+    return db.posts.filter(post => {
+      const isTitleMatch = post.title.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+      const isBodyMatch = post.body.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+      return isTitleMatch || isBodyMatch
+    })
+  },
+
+  comments(parent, args, { db }, info) {
+    return db.comments
+  },
+
+  me() {
+    return {
+      id: '1234',
+      name: 'Mike',
+      email: 'wes@gmail.com',
+      age: 20
+    }
+  },
+
+  post() {
+    return {
+      id: '092',
+      title: 'GraphQL 101',
+      body: 'This is a body',
+      published: false
+    }
+  }
+}
+
+export { Query as default }
