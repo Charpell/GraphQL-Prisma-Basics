6f853f705cf237a2b94bde23b63628290a6182cb
Charpell <charpellumeh@gmail.com>
2018-12-06 08:39:19 +0100

    Using prisma binding to get all the users and comments

--- graphql-prisma/prisma.js
+++
@@ -1,6 +0,0 @@
-import { Prisma } from 'prisma-binding'
-
-const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'localhost:4466'
-})
---
+++ graphql-prisma/src/prisma.js
@@ -0,0 +1,17 @@
+import { Prisma } from 'prisma-binding'
+
+const prisma = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466'
+})
+
+prisma.query.users(null, '{ id name posts { id title } }')
+  .then(data => {
+    console.log(JSON.stringify(data, undefined, 2))
+  })
+
+
+prisma.query.comments(null, '{ id text author { id name } }')
+  .then(data => {
+    console.log(JSON.stringify(data, undefined, 2))
+  })
