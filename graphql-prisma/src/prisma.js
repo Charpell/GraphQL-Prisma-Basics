176a7ee2afd542b19b3cee90866e3813c304d452
Charpell <charpellumeh@gmail.com>
2018-12-06 09:00:01 +0100

    Mutations with Prisma Binding

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,13 +5,47 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name posts { id title } }')
-  .then(data => {
-    console.log(JSON.stringify(data, undefined, 2))
-  })
+// prisma.query.users(null, '{ id name posts { id title } }')
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2))
+//   })
 
 
-prisma.query.comments(null, '{ id text author { id name } }')
-  .then(data => {
-    console.log(JSON.stringify(data, undefined, 2))
-  })
+// prisma.query.comments(null, '{ id text author { id name } }')
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 2))
+//   })
+
+
+prisma.mutation.createPost({
+  data: {
+    title: 'Title of Post',
+    body: 'New body of post',
+    published: false,
+    author: {
+      connect: {
+        id: "cjpc7xjf4001s0811g4hypnjg"
+      }
+    }
+  }
+}, '{ id title body published }').then(data => {
+  console.log(data)
+  return prisma.query.users(null, '{ id name posts { id title } }')
+}).then(data => {
+  console.log(JSON.stringify(data, undefined, 2))
+})
+
+
+prisma.mutation.updatePost({
+  where: {
+    id: "cjpcaztqw0053081127pmol5q"
+  },
+  data: {
+    body: "Newly updated post",
+    published: true
+  }
+}, '{ id }').then(data => {
+  return prisma.query.posts(null, '{ id title body published }')
+}).then(data => {
+  console.log(data)
+})
