306ed8f7f03ca9ae30198ce1703432528ab983a5
Charpell <charpellumeh@gmail.com>
2018-12-09 06:53:26 +0100

    Pagination with Cusors

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -4,7 +4,8 @@ const Query = {
   users(parent, args, { prisma }, info) {
     const opArgs = {
       first: args.first,
-      skip: args.skip
+      skip: args.skip,
+      after: args.after
     }
 
     if (args.query) {
@@ -23,6 +24,9 @@ const Query = {
   myPosts(parent, args, { prisma, request }, info) {
     const userId = getUserId(request)
     const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
       where: {
         author: {
           id: userId
@@ -43,6 +47,7 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
+      after: args.after,
       where: {
         published: true
       }
@@ -60,7 +65,13 @@ const Query = {
   },
 
   comments(parent, args, { prisma }, info) {
-    return prisma.query.comments(null, info)
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after
+    }
+    
+    return prisma.query.comments(opArgs, info)
   },
 
   me(parent, args, { prisma, request }, info) {
