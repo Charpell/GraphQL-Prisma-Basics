41aabe694ffa0a25153883b19f1fc80a087adba9
Charpell <charpellumeh@gmail.com>
2018-12-09 08:25:11 +0100

    Production Deployment

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'
 import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 import db from './db';
@@ -21,6 +22,6 @@ const server = new GraphQLServer({
   fragmentReplacements
 })
 
-server.start(() => {
+server.start({port: process.env.PORT || 4000}, () => {
   console.log('The server is up!')
 })
