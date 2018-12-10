c30a8134688a0d4ba2057c486e012a1339bb8305
Charpell <charpellumeh@gmail.com>
2018-12-10 05:40:34 +0100

    Seeding the database with Test data

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,13 +1,51 @@
 import 'cross-fetch/polyfill';
 import ApolloBoost, { gql } from 'apollo-boost';
+import bcrypt from 'bcryptjs'
 import prisma from '../src/prisma'
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000'
 })
 
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts()
+  await prisma.mutation.deleteManyUsers()
+  await prisma.mutation.createUser({
+    data: {
+      name: 'Jen',
+      email: 'jen@example.com',
+      password: bcrypt.hashSync('Red098!@#$')
+    }
+  })
 
-testMap('Should create a new user', async () => {
+  await prisma.mutation.createPost({
+    data: {
+      title: 'My published post',
+      body: '',
+      published: true,
+      author: {
+        contact: {
+          id: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'My draft post',
+      body: '',
+      published: false,
+      author: {
+        contact: {
+          id: user.id
+        }
+      }
+    }
+  })
+})
+
+test('Should create a new user', async () => {
   const createUser = gql`
     mutation {
       createUser(
