bbff438b5f65f1c52ac4dc5466ff66f9cd4ec74b
Charpell <charpellumeh@gmail.com>
2018-12-10 05:30:35 +0100

    Testing Mutations

---
+++ graphql-prisma/tests/user.test.js
@@ -0,0 +1,34 @@
+import 'cross-fetch/polyfill';
+import ApolloBoost, { gql } from 'apollo-boost';
+import prisma from '../src/prisma'
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
+})
+
+
+testMap('Should create a new user', async () => {
+  const createUser = gql`
+    mutation {
+      createUser(
+        data: {
+          name: "Andrew",
+          email: "andrew@example.com",
+          password: "MyPass123"
+        }
+      ){
+        token,
+        user {
+          id
+        }
+      }
+    }
+  `
+
+  const response = await client.mutate({
+    mutation: createUser
+  })
+  
+  const exists = await prisma.exists.User({ id: response.data.createUser.user.id })
+  expect(exists).toBe(true)
+})
